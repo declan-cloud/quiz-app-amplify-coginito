87533e4363ba5ffd4869224d0a420baf87e72a30
declan-cloud <declanonuoha18@gmail.com>
2024-06-06 01:29:22 +0300

    Update quizData.js
    Removed a question

--- src/quizData.js
+++ src/quizData.js
@@ -5,11 +5,6 @@ const quizData = [
       answer: "Spacewar!"
     },
     {
-      question: "Which company developed the first commercial antivirus software?",
-      options: ["Symantec", "McAfee", "Norton", "Kaspersky Lab"],
-      answer: "McAfee"
-    },
-    {
       question: "Which animal is featured in the official PHP logo?",
       options: ["Elephant", "Hippo", "Giraffe", "Lion"],
       answer: "Elephant"
